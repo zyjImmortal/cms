87a7de0beff9b96b29481ae666c95c1564fea4e6
zhouyajun <zyj665582>
2019-11-21 16:13:07 +0800

    添加动态规划

---
+++ cms-core/src/main/java/com/zyj/cms/core/service/book/logic/container/StringDemo.java
@@ -0,0 +1,27 @@
+package com.zyj.cms.core.service.book.logic.container;
+
+import com.zyj.cms.util.commons.StringUtil;
+
+import java.util.Arrays;
+
+/**
+ * Author: zhouyajun
+ * Date: 2019-10-25
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        String a = new String("ab"); // a 为一个引用
+        String b = new String("ab"); // b为另一个引用,对象的内容一样
+        String aa = "ab"; // 放在常量池中
+        String bb = "ab"; // 从常量池中查找
+        if (aa == bb) // true
+            System.out.println("aa==bb");
+        if (a == b) // false，非同一对象
+            System.out.println("a==b");
+        if (a.equals(b)) // true
+            System.out.println("aEQb");
+        if (42 == 42.0) { // true
+            System.out.println("true");
+        }
+    }
+}
--- cms-core/src/main/java/com/zyj/cms/core/service/geek/MergeQuickSort.java
+++ cms-core/src/main/java/com/zyj/cms/core/service/geek/MergeQuickSort.java
@@ -13,10 +13,10 @@ public class MergeQuickSort {
     }
 
     public static void mergeSort(int[] nums, int p, int r) {
-        if (p > r) {
+        if (p >= r) {
             return;
         }
-        int mid = (p + r) / 2;
+        int mid = p + (p + r) / 2;
         mergeSort(nums, p, mid);
         mergeSort(nums, mid + 1, r);
         merge(nums, p, mid, r);
@@ -50,6 +50,23 @@ public class MergeQuickSort {
         }
         for (; p <= r; p++) {
             nums[p] = temp[p];
+        }
+    }
+
+    public static void mergerV2(int[] nums, int p, int mid, int r) {
+        int[] copy = nums.clone();
+        // 定义一个指针表示从什么位置开始修改原来的数组,i 指针表示左半边的起始位置，j 表示右半边的起始位置
+        int k = p, i = p, j = mid + 1;
+        while (k <= r) {
+            if (i > mid) {
+                nums[k++] = copy[j++];
+            } else if (j >= r) {
+                nums[k++] = copy[i++];
+            } else if (nums[j] < copy[i]) {
+                nums[k++] = nums[j++];
+            } else {
+                nums[k++] = nums[i++];
+            }
         }
     }
 }
